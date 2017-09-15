1614553502bdfb3ffbba725be17097819d98379d
Oguz BAKIR <b21627007@cs.hacettepe.edu.tr>
2017-09-15 03:36:12 +0300

    Nearly working neural network and input generator added

---
+++ generator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+  // Random training sets for XOR -- two inputs and one output
+
+  srand (time(NULL));
+
+  std::cout << "topology: 2 4 1" << '\n';
+  for (int i = 20000; i >=0; i--){
+    int n1 = (int)(2.0 * rand() / double(RAND_MAX));
+    int n2 = (int)(2.0 * rand() / double(RAND_MAX));
+    int t = n1 ^ n2; //Should be 0 or 1
+    cout << "in: " << n1 << ".0 " << n2 << ".0 " << endl;
+    cout << "out: " << t << ".0" << endl;
+  }
+
+  return 0;
+}
